3860001b69cd8f0875904875360481e20fbb9106
SayaKurma <ridzi1277@gmail.com>
2025-03-11 20:22:02 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 const navToggle = document.getElementById('navToggle');
-const navMenu = document.querySelector('nav');
+const navMenu = document.getElementById('navMenu');
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     navToggle.classList.toggle('active');
+});
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+        navToggle.classList.remove('active');
+    });
 });
